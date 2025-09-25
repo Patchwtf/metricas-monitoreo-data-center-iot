70896e2df466217beebf55c86225e872054b7d22
“Patchwtf” <jhosbyndaniel@gmail.com>
2025-09-25 14:07:32 -0600

    Creación de Proyecto. Adicion de librerias

---
+++ src/main/java/com/metricas_monitoreo_data_center_iot/com/demo/MetricasMonitoreoDataCenterIotApplication.java
@@ -0,0 +1,13 @@
+package com.metricas_monitoreo_data_center_iot.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricasMonitoreoDataCenterIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricasMonitoreoDataCenterIotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metricas_monitoreo_data_center_iot/com/demo/MetricasMonitoreoDataCenterIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metricas_monitoreo_data_center_iot.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricasMonitoreoDataCenterIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
